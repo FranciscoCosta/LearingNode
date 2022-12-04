4c94a88673b6e01397bcda1c903b6eb3288bf87a
Xico <francisco100eg@gmail.com>
2022-12-04 18:34:35 -0300

    :Intro to Globals in Node

---
+++ 2-Globals.js
@@ -0,0 +1,16 @@
+//Globals
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use module
+// module - info about current module
+// process - info about env wher the pogram is being excuted
+
+// console.log(__dirname);
+setInterval(() => {
+    console.log("HI")
+}, 100);
+// console.log(__filename);
+// console.log(require);
+// console.log(module);
+// console.log(process);
